e8c90a6513f98065786f110055037079e665f934
programmingalex2019 <programmingalex2019@gmail.com>
2021-03-07 14:58:53 +0200

    Trying to find second camera

--- javascript/questions.js
+++ javascript/questions.js
@@ -31,7 +31,7 @@ function openCamera(){
 
     Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
-            scanner.start(cameras[1]);
+            scanner.start(cameras[0]);
             for(let i = 0; i < cameras.length; i++){
                 if(cameras[i].name.indexOf('back') !== -1){
                     scanner.start(cameras[i]);
